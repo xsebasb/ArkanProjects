dffe1257346853c94626adc84ee533e474b1f48c
xsebasb <61366649+xsebasb@users.noreply.github.com>
2022-08-08 11:19:25 -0500

    Proyecto Spring Backend
    Proyecto backend en Spring Boot

---
+++ arkan-crud-backend/arkan-crud-backend/src/main/java/com/arkan/crud/backend/ArkanCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arkan.crud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkanCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkanCrudBackendApplication.class, args);
+	}
+
+}
---
+++ arkan-crud-backend/arkan-crud-backend/src/test/java/com/arkan/crud/backend/ArkanCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkan.crud.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkanCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
