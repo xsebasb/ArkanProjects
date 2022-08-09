f4a82f2f0ef5c5b33c4aa7be421bf993e44236f9
xsebasb <61366649+xsebasb@users.noreply.github.com>
2022-08-09 08:53:29 -0500

    Eliminacion otro proyecto

--- arkan-crud-backend/PromoXalapaBack-main/src/main/java/com/sistema/blog/repositorio/PublicacionRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.blog.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sistema.blog.entidades.Publicacion;
-
-public interface PublicacionRepositorio extends  JpaRepository<Publicacion, Long>{
-
-}
