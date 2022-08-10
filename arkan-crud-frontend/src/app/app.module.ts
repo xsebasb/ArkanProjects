9190b05296f02446a30355db769d3378eb640dcc
xsebasb <61366649+xsebasb@users.noreply.github.com>
2022-08-10 13:51:36 -0500

    Implementando frontend con angular
    se realizo las card utilizando la libreria bootstrap

--- arkan-crud-frontend/src/app/app.module.ts
+++ arkan-crud-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
